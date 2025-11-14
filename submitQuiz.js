352a1a550f87f09c8014406f1a41c902b48230a0
ceomsf786-bit <ceomsf786@gmail.com>
2025-11-14 22:03:42 +0200

    Update submitQuiz.js

--- submitQuiz.js
+++ submitQuiz.js
@@ -1,5 +1,5 @@
 function submitQuiz(data){
-  fetch("https://script.google.com/macros/s/AKfycbzIOW_m-xvpQ-HfKSULtsk7LsQWpLoKeG8eiEgdFsI/dev", {
+  fetch("https://script.google.com/macros/s/AKfycbz9oGOePvA0i8uwOV30KUG6uqy_es0zWhVOnz6_ltmkVnFKvNJPfXANV-2mBCTr7nrr/exec", {
     method: "POST",
     body: JSON.stringify(data)
   })
